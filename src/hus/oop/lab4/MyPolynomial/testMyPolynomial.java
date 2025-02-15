7be1c5fbfdc5d9e8be3da82ce2ff8e98854a33cf
Nguyễn Cảnh Tùng <nguyencanhtung@MacBook-Air-cua-Nguyen.local>
2025-02-15 10:56:24 +0700

    delete zip file, rename package

--- src/hus/oop/lab4/MyPolynomial/testMyPolynomial.java
+++
@@ -1,7 +0,0 @@
-package hus.oop.lab4.MyPolynomial;
-
-public class testMyPolynomial {
-    public static void main(String[] args) {
-        System.out.println(new MyPolynomial(1.1, 2.2, 3.3, 4.4, 5.5).toString());
-    }
-}
---
+++ src/hus/oop/lab4/mypolynomial/testMyPolynomial.java
@@ -0,0 +1,7 @@
+package hus.oop.lab4.mypolynomial;
+
+public class testMyPolynomial {
+    public static void main(String[] args) {
+        System.out.println(new MyPolynomial(1.1, 2.2, 3.3, 4.4, 5.5).toString());
+    }
+}
